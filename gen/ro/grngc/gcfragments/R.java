0125ea24b51f6c81a7dacdb7045bea340a286b6a
GC Solutions <grngc.solutions@gmail.com>
2013-01-30 01:52:38 +0200

    A couple of Fragments opening one eachother... All Working fine.

--- gen/ro/grngc/gcfragments/R.java
+++ gen/ro/grngc/gcfragments/R.java
@@ -12,20 +12,45 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo_tarnava_mare=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int ContainerLayout=0x7f070003;
+        public static final int article_fragment=0x7f070007;
+        public static final int button=0x7f070002;
+        public static final int headlines_fragment=0x7f070005;
+        public static final int imageView1=0x7f070006;
+        public static final int main_fragment=0x7f070000;
+        public static final int menu_settings=0x7f070008;
+        public static final int normalTitle=0x7f070004;
+        public static final int textView1=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_news_fragments=0x7f030001;
+        public static final int article_view=0x7f030002;
+        public static final int fragment_main=0x7f030003;
+        public static final int headlines_view=0x7f030004;
+        public static final int news_articles=0x7f030005;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
+        public static final int activity_news_fragments=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int article_detailed_view=0x7f04000b;
+        public static final int articles_headlines_view=0x7f04000c;
+        public static final int click_me=0x7f040004;
+        public static final int close=0x7f040008;
+        public static final int continue_frames=0x7f040007;
+        public static final int dismiss=0x7f040009;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int simple_info=0x7f040005;
+        public static final int simple_message=0x7f040006;
+        public static final int tarnavamare_logo=0x7f040003;
+        public static final int title_activity_news_fragments=0x7f04000a;
     }
     public static final class style {
         /** 
--- src/ro/grngc/gcfragments/MainActivity.java
+++ src/ro/grngc/gcfragments/MainActivity.java
@@ -1,13 +1,52 @@
 package ro.grngc.gcfragments;
 
+import ro.grngc.gcfragments.MainFragment.UiListener;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
-public class MainActivity extends FragmentActivity {
-
+public class MainActivity extends FragmentActivity implements UiListener{
+    @SuppressWarnings("unused")
+	private MainFragment fragment;
+	private AlertDialog emptyTaskDialog;
+    @Override
+    protected void onCreate(Bundle arg0) {
+            super.onCreate(arg0);
+            setContentView(R.layout.activity_main);
+            fragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.main_fragment);
+    }
+    public void onButtonClicked(View view){
+          // handle button clicked
+    	emptyTaskDialog = new AlertDialog.Builder(this)
+		.setTitle(R.string.simple_info)
+		.setMessage(R.string.simple_message)
+		.setPositiveButton(R.string.continue_frames, new AlertDialog.OnClickListener(){
+			public void onClick(DialogInterface dialog, int which) {
+				 openMoreFragments();
+			}
+		})
+		.setNeutralButton(R.string.dismiss, new AlertDialog.OnClickListener(){
+			public void onClick(DialogInterface dialog, int which) {
+				emptyTaskDialog.cancel();
+			}
+		})
+		.setNegativeButton(R.string.close, new AlertDialog.OnClickListener(){
+			public void onClick(DialogInterface dialog, int which) {
+				finish();
+			}
+		}).create();
+    	emptyTaskDialog.show();
+    }
+	protected void openMoreFragments() {
+		 Intent intent = new Intent(this, NewsFragmentsActivity.class);
+		 startActivity(intent);
+	}
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.news_articles);
+	public void onButtonClicked() {
+		// TODO Auto-generated method stub
+		
 	}
 }
